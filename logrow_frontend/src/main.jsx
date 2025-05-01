091fcceedcf21a9050d4718f656addc41312c6b9
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-01 19:17:09 +0900

    initial commit

--- logrow_frontend/src/main.jsx
+++ logrow_frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      // {
+      //   path: "login",
+      //   Component: Login,
+      // },
+
+    ],
+  },
+  
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
