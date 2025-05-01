bab5a00293d8211cb00c894afc589906737c1f4c
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-02 03:24:13 +0900

    style: 전체 css 추가

--- logrow_frontend/src/pages/addStudy/index.jsx
+++ logrow_frontend/src/pages/addStudy/index.jsx
@@ -1,41 +1,54 @@
 import styles from "./AddStudy.module.css";
 import Sidebar from "../Sidebar";
+import logo from "../../assets/logo.svg";
 
 export default function AddStudy() {
+  const days = [
+    { kor: "월요일", eng: "Mon." },
+    { kor: "화요일", eng: "Tue." },
+    { kor: "수요일", eng: "Wed." },
+    { kor: "목요일", eng: "Thu." },
+    { kor: "금요일", eng: "Fri." },
+    { kor: "토요일", eng: "Sat." },
+    { kor: "일요일", eng: "Sun." },
+  ];
+
   return (
     <div className={styles.container}>
       <Sidebar />
+      <img src={logo} alt="Logo" className={styles.logo} />
 
       <div className={styles.formContainer}>
-      <div className={styles.header}>스터디 개설하기</div>
+        <div className={styles.header}>스터디 개설하기</div>
 
-      
         <form className={styles.form}>
-          <label>
-            스터디명
-            <input type="text" name="studyName" />
-          </label>
+          <div className={styles.firstLabel}>
+            <div className={styles.inputContainer}>
+              <span className={styles.label}>스터디명</span>
+              <input type="text" name="studyName" />
+            </div>
 
-          <label>
-            스터디 유형
-            <select name="studyType">
-              <option value="프로젝트">프로젝트형</option>
-              <option value="스터디">스터디형</option>
-              <option value="챌린지">챌린지형</option>
-              <option value="토론">토론형</option>
-              <option value="실습">실습형</option>
-              <option value="멘토링">멘토링형</option>
-              <option value="모의면접">모의면접형</option>
-            </select>
-          </label>
+            <div className={styles.inputContainer}>
+              <span className={styles.label}>스터디 유형</span>
+              <select name="studyType">
+                <option value="프로젝트">프로젝트형</option>
+                <option value="스터디">스터디형</option>
+                <option value="챌린지">챌린지형</option>
+                <option value="토론">토론형</option>
+                <option value="실습">실습형</option>
+                <option value="멘토링">멘토링형</option>
+                <option value="모의면접">모의면접형</option>
+              </select>
+            </div>
 
-          <label>
-            스터디 모집 인원
-            <input type="number" name="studyParticipants" min="0" />
-          </label>
+            <div className={styles.inputContainer}>
+              <span className={styles.label}>스터디 모집 인원</span>
+              <input type="number" name="studyParticipants" min="0" />
+            </div>
+          </div>
 
-          <div>
-            <div className={styles.label}>스터디 모집 기간:</div>
+          <div className={styles.inputContainer}>
+            <span className={styles.label}>스터디 모집 기간</span>
             <div className={styles.dateRange}>
               <input type="date" name="startDate" />
               <span>~</span>
@@ -43,46 +56,34 @@ export default function AddStudy() {
             </div>
           </div>
 
-          <label>
-            진행 요일
-            <div className={styles.checkboxGroup}>
-              <label>
-                <input type="checkbox" name="day" value="월" /> 월
-              </label>
-              <label>
-                <input type="checkbox" name="day" value="화" /> 화
-              </label>
-              <label>
-                <input type="checkbox" name="day" value="수" /> 수
-              </label>
-              <label>
-                <input type="checkbox" name="day" value="목" /> 목
-              </label>
-              <label>
-                <input type="checkbox" name="day" value="금" /> 금
-              </label>
-              <label>
-                <input type="checkbox" name="day" value="토" /> 토
-              </label>
-              <label>
-                <input type="checkbox" name="day" value="일" /> 일
-              </label>
+          <div className={styles.inputContainer}>
+            <span className={styles.label}>진행 요일</span>
+            <div className={styles.dayButtonGroup}>
+              {days.map((day, idx) => (
+                <label key={idx} className={styles.dayButton}>
+                  <input type="checkbox" name="day" value={day.kor.slice(0, 1)} />
+                  <div className={styles.dayButtonContent}>
+                    <span className={styles.korean}>{day.kor}</span>
+                    <span className={styles.english}>{day.eng}</span>
+                  </div>
+                </label>
+              ))}
             </div>
-          </label>
+          </div>
 
-          <label>
-            진행 방식
+          <div className={styles.inputContainer}>
+            <span className={styles.label}>진행 방식</span>
             <select name="mode">
               <option value="온라인">온라인</option>
               <option value="오프라인">오프라인</option>
               <option value="혼합">혼합</option>
             </select>
-          </label>
+          </div>
 
-          <label>
-            스터디 설명
+          <div className={styles.inputContainer}>
+            <span className={styles.label}>스터디 설명</span>
             <textarea name="studyDescription" rows="4" />
-          </label>
+          </div>
 
           <button type="submit">신청하기</button>
         </form>
