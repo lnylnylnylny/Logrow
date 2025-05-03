5c7784291faaa05de465be1c5ff04fcbc16be594
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-03 20:21:34 +0900

    feat: 기본 구조적 틀 제작

---
+++ logrow_frontend/src/pages/Mystudy/components/NavigationTabs/index.jsx
@@ -0,0 +1,7 @@
+export default function NavigationTabs() {
+  return (
+    <div>
+      <h1>Navigation Tabs</h1>
+    </div>
+  );
+}
--- logrow_frontend/src/pages/Mystudy/index.jsx
+++ logrow_frontend/src/pages/Mystudy/index.jsx
@@ -1,9 +1,20 @@
-import Sidebar from "../Sidebar"
+import Sidebar from "../Sidebar";
+import NavigationTabs from "./components/NavigationTabs";
+import styles from "./Mystudy.module.css";
 
 export default function MyStudy() {
   return (
-    <div>
-        <Sidebar />
+    <div className={styles.container}>
+      <Sidebar />
+
+      <div className={styles.contentWrapper}>
+        <NavigationTabs />
+
+        <div className={styles.mainContent}>
+          {/* 👉 여기에서 각 화면 (Room, Checklist 등)을 렌더링 */}
+            내용
+        </div>
+      </div>
     </div>
   );
 }
