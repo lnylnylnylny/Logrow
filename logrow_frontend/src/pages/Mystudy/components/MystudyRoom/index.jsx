efa9a560257744c4e1414cc6fa068e7767434804
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-03 21:04:58 +0900

    feat: study room UI 화면 제작 + 틀 설정

--- logrow_frontend/src/pages/Mystudy/components/MystudyRoom/index.jsx
+++ logrow_frontend/src/pages/Mystudy/components/MystudyRoom/index.jsx
@@ -1,8 +1,21 @@
+import styles from "./MystudyRoom.module.css";
+import Bookmark from "../../../../assets/bookmark.svg";
+
 export default function MystudyRoom() {
   return (
-    <div>
-      <h1>My Study Room</h1>
-      <p>Welcome to your study room!</p>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <img src={Bookmark} alt="Bookmark" className={styles.icon} />
+        <div className={styles.title}>My Study Room</div>
+      </div>
+
+      <div className={styles.grid}>
+        {/* 추후 RoomCard 컴포넌트 반복 삽입 */}
+        <div className={styles.card}>스터디 카드</div>
+        <div className={styles.card}>스터디 카드</div>
+        <div className={styles.card}>스터디 카드</div>
+        <div className={styles.card}>스터디 카드</div>
+      </div>
     </div>
   );
 }
